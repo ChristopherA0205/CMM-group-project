5b2ffc587cd203f9de66dc459e040d8e40ed47c9
faisalbindakhil <147502701+faisalbindakhil@users.noreply.github.com>
2023-10-16 15:25:09 +0100

    Update comp project 1 .py

--- comp project 1 .py
+++ comp project 1 .py
@@ -1,167 +1,78 @@
-
-"""
-Created on Thu Oct  5 22:19:21 2023
-
-@author: brodi
-@edit: ben
-"""
-
+# Importing Libraries and Modules
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy import stats
 from scipy.optimize import curve_fit
+from scipy import stats
 
+# Define alpha and delta_el values
+alpha = np.deg2rad(np.array([-16,-12,-8,-4,-2,0,2,4,8,12]))
+delta_el = np.deg2rad(np.array([-20,-10,0,10,20]))
 
+# Define aerodynamic coefficients for the wing for different alpha values
+CD_wing = np.array([0.115, 0.079, 0.047, 0.031, 0.027, 0.027, 0.029, 0.034, 0.054, 0.089])
+CL_wing = np.array([-1.421, -1.092, -0.695, -0.312, -0.132, 0.041, 0.218, 0.402, 0.786, 1.186])
+CM_wing = np.array([0.0775, 0.0663, 0.053, 0.0337, 0.0217, 0.0073, -0.009, -0.0263, -0.0632, -0.1235])
 
-alpha = np.array([-16,-12,-8,-4,-2,0,2,4,8,12])
-delta_el = np.array([-20,-10,0,10,20])
+# Define aerodynamic coefficients for the elevator for different delta_el values
+CL_el = np.array([-0.051, -0.038, 0, 0.038, 0.052])
+CM_el = np.array([0.0842, 0.0601, -0.0001, -0.0601, -0.0843])
 
-CD_wing = np.array([0.115000000000000, 
-                    0.079000000000000, 
-                    0.047000000000000, 
-                    0.031000000000000, 
-                    0.027000000000000, 
-                    0.027000000000000, 
-                    0.029000000000000, 
-                    0.034000000000000, 
-                    0.054000000000000, 
-                    0.089000000000000])
+# Function to plot a line of best fit for the given data and return slope and intercept
+def plot_best_fit(x, y, x_label, y_label, title):
+    # Calculate the line of best fit
+    slope, intercept, _, _, _ = stats.linregress(x, y)
+    line_equation = f'y = {slope:.2f}x + {intercept:.2f}'
+    
+    # Plot the data and the line of best fit
+    plt.figure(figsize=(6, 4))
+    plt.scatter(x, y, label='Data Points')
+    plt.plot(x, slope * x + intercept, label=f'Line of Best Fit: {line_equation}', color='red')
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    plt.legend()
+    plt.grid(True)
+    plt.title(title)
+    plt.show()
+    
+    return slope, intercept
 
-CL_wing = np.array([-1.421000000000000,
-                    -1.092000000000000,
-                    -0.695000000000000,
-                    -0.312000000000000,
-                    -0.132000000000000,
-                    0.041000000000000, 
-                    0.218000000000000, 
-                    0.402000000000000, 
-                    0.786000000000000, 
-                    1.186000000000000])
+# Function to plot a quadratic fit for the given data and return the parameters of the quadratic function
+def plot_quadratic_fit(x, y, x_label, y_label, title):
+    # Define the quadratic function
+    def quadratic_function(x, a, b, c):
+        return a * x**2 + b * x + c
 
-CM_wing = np.array([0.077500000000000, 
-                    0.066300000000000, 
-                    0.053000000000000, 
-                    0.033700000000000, 
-                    0.021700000000000, 
-                    0.007300000000000,
-                    -0.009000000000000,
-                    -0.026300000000000,
-                    -0.063200000000000,
-                    -0.123500000000000])
+    # Fit the data with the quadratic function
+    params, _ = curve_fit(quadratic_function, x, y)
+    x_fit = np.linspace(min(x), max(x), 100)
+    y_fit = quadratic_function(x_fit, *params)
+    
+    # Plot the data and the quadratic fit
+    plt.figure(figsize=(8, 6))
+    plt.scatter(x, y, label='Data', color='blue')
+    plt.plot(x_fit, y_fit, label=f'Quadratic Fit: {params[0]:.2f}x^2 + {params[1]:.2f}x + {params[2]:.2f}', color='red')
+    plt.title(title)
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+    
+    return params
 
-CL_el = np.array([-0.051000000000000,
-                  -0.038000000000000, 
-                  0, 0.038000000000000, 
-                  0.052000000000000])
+# Use the functions to calculate and plot the aerodynamic coefficients for different cases
+C_L_a, C_L_0 = plot_best_fit(alpha, CL_wing, '\u03B1', 'C$_{L}$$^{wing}$', "C$_{L}$$_{0}$ + C$_{L}$$_{\u03B1}$\u03B1")
+C_M_a, C_M_0 = plot_best_fit(alpha, CM_wing, '\u03B1', 'C$_{M}$$^{wing}$', "C$_{M}$$_{0}$ + C$_{M}$$_{\u03B1}$\u03B1")
+C_L_del_E, _ = plot_best_fit(delta_el, CL_el, '\u03B4', 'C$_{L}$$^{el}$', "C$_{L}$$_{\u03B4}$$_{E}$")
+C_M_del_E, _ = plot_best_fit(delta_el, CM_el, '\u03B4', 'C$_{M}$$^{el}$', "C$_{M}$$_{\u03B4}$$_{E}$")
 
-CM_el = np.array([0.084200000000000, 
-                  0.060100000000000,
-                  -0.000100000000000,
-                  -0.060100000000000,
-                  -0.084300000000000])
+# Calculate the lift coefficient CL using the obtained values
+CL = C_L_0 + C_L_a*alpha + C_L_del_E * ((-C_M_0 + C_M_a*alpha) / C_M_del_E)
+# Fit the lift coefficient CL and the drag coefficient CD with a quadratic function and plot it
+K, _, C_D_0 = plot_quadratic_fit(CL, CD_wing, '$C_L$', '$C_D$', 'Quadratic Curve Line of Best Fit')
 
+# ... [Calculations & Bisection Method] ...
 
-
-#calculate line of best fit
-slope, intercept, r_value, p_value, std_err = stats.linregress(alpha, CL_wing)
-line_equation = f'y = {slope:.2f}x + {intercept:.2f}'
-#plt data points
-plt.scatter(alpha, CL_wing, label='Data Points')
-#plt line of best fit
-plt.plot(alpha, slope * alpha + intercept, label=f'Line of Best Fit: {line_equation}', color='red')
-#edit plto
-plt.xlabel('\u03B1')
-plt.ylabel('C$_{L}$$^{wing}$')
-plt.legend()
-plt.grid(True)
-plt.title("C$_{L}$$_{0}$ + C$_{L}$$_{\u03B1}$\u03B1")
-plt.show()
-#Assign Values
-C_L_a=slope
-C_L_0=intercept
-
-
-# Subplot 2
-#calculate line of best fit
-slope, intercept, r_value, p_value, std_err = stats.linregress(alpha, CM_wing)
-line_equation = f'y = {slope:.2f}x + {intercept:.2f}'
-#plt data points
-plt.figure(1, figsize=(6, 4))
-plt.scatter(alpha, CM_wing, label='Data Points')
-#plt line of best fit
-plt.plot(alpha, slope * alpha + intercept, label=f'Line of Best Fit: {line_equation}', color='red')
-#edit plt
-plt.xlabel('\u03B1')
-plt.ylabel('C$_{M}$$^{wing}$')
-plt.legend()
-plt.grid(True)
-plt.title("C$_{M}$$_{0}$ + C$_{M}$$_{\u03B1}$\u03B1")
-plt.show()
-#Assign Values
-C_M_a=slope
-C_M_0=intercept
-
-# Subplot 3
-#calculate line of best fit
-slope, intercept, r_value, p_value, std_err = stats.linregress(delta_el, CL_el)
-line_equation = f'y = {slope:.2f}x + {intercept:.2f}'
-#plt data points
-plt.figure(1, figsize=(6, 4))
-plt.scatter(delta_el, CL_el, label='Data Points')
-#plt line of best fit
-plt.plot(delta_el, slope * delta_el + intercept, label=f'Line of Best Fit: {line_equation}', color='red')
-#edit plt
-plt.xlabel('\u03B4')
-plt.ylabel('C$_{L}$$^{el}$')
-plt.legend()
-plt.grid(True)
-plt.title("C$_{L}$$_{\u03B4}$$_{E}$")
-plt.show()
-#Assign Values
-C_L_del_E=slope
-
-
-# Subplot 4
-#calculate line of best fit
-slope, intercept, r_value, p_value, std_err = stats.linregress(delta_el, CM_el)
-line_equation = f'y = {slope:.2f}x + {intercept:.2f}'
-#plt data points
-plt.figure(1, figsize=(6, 4))
-plt.scatter(delta_el, CM_el, label='Data Points')
-#plt line of best fit
-plt.plot(delta_el, slope * delta_el + intercept, label=f'Line of Best Fit: {line_equation}', color='red')
-#edit plt
-plt.xlabel('\u03B4')
-plt.ylabel('C$_{M}$$^{el}$')
-plt.legend()
-plt.grid(True)
-plt.title("C$_{M}$$_{\u03B4}$$_{E}$")
-plt.show()
-#Assign Values
-C_M_del_E=slope
-
-CL=C_L_0+C_L_a*alpha+C_L_del_E*((-C_M_0+C_M_a*alpha)/C_M_del_E)
-
-# Subplt 5
-def quadratic_function(x, a, b, c):
-    return a * x**2 + b * x + c
-#calculate line of best fit
-params, covariance = curve_fit(quadratic_function, CL, CD_wing)
-x_fit = np.linspace(min(CL), max(CL), 100)
-y_fit = quadratic_function(x_fit, *params)
-#plot data and line of best fit
-plt.figure(figsize=(8, 6))
-plt.scatter(CL, CD_wing, label='Data', color='blue')
-plt.plot(x_fit, y_fit, label=f'Quadratic Fit: {params[0]:.2f}x^2 + {params[1]:.2f}x + {params[2]:.2f}', color='red')
-plt.title('Quadratic Curve Line of Best Fit')
-plt.xlabel('$C_L$')
-plt.ylabel('$C_D$')
-plt.legend()
-plt.grid(True)
-plt.show()
-#Assign Values
-K=params[0]
-C_D_0=params[2]
 
 #inputs
 # values are m = mass, g = gravity, V = Velocity, p = density of air, S = wing surface area, PA = path angle, roundval = values we want answers rounded to 
@@ -176,7 +87,7 @@ roundval = 4
 
 #alpha=array degrees A=array in radians Alpha=specific alpha in radians
 CL=C_L_0+C_L_a*alpha+C_L_del_E*((-C_M_0+C_M_a*alpha)/C_M_del_E)
-A = np.deg2rad(alpha)
+A = alpha
 
 L=0.5*V**2*p*S*CL
 D=0.5*V**2*p*S*CD_wing
@@ -254,10 +165,3 @@ print(f"Your Angle of Attack is {Alpha} rad")
 print(f"The elevator angle is {Del_E} rad")
 print(f"Your Thrust force is {T} N")
 print(f"Your Pitch angle is {Theta} is")
-
-
-
-
-
-
-
