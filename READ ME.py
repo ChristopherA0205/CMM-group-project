8757af7317883eb19da2ffa3ec9c5cb49b965e08
faisalbindakhil <147502701+faisalbindakhil@users.noreply.github.com>
2023-11-09 19:35:20 +0000

    Add files via upload

---
+++ READ ME.py
@@ -0,0 +1,73 @@
+'''
+  ____                              _  _____   
+ / ___| _ __   ___   _   _  _ __   / ||___  |  
+| |  _ | '__| / _ \ | | | || '_ \  | |   / /   
+| |_| || |   | (_) || |_| || |_) | | |  / /    
+ \____||_|    \___/  \__,_|| .__/  |_| /_/     
+                           |_|                 
+ _____  _  _         _      _    
+|  ___|| |(_)  __ _ | |__  | |_  
+| |_   | || | / _` || '_ \ | __| 
+|  _|  | || || (_| || | | || |_  
+|_|    |_||_| \__, ||_| |_| \__| 
+              |___/              
+ ____   _                    _         _                
+/ ___| (_) _ __ ___   _   _ | |  __ _ | |_   ___   _ __ 
+\___ \ | || '_ ` _ \ | | | || | / _` || __| / _ \ | '__|
+ ___) || || | | | | || |_| || || (_| || |_ | (_) || |   
+|____/ |_||_| |_| |_| \__,_||_| \__,_| \__| \___/ |_|   
+
+                               ________|________
+                              "-----------------"
+                                    || | ||
+                                    ||---||
+                                    ' ___ '
+                          ---------'  .-.  '---------
+          _________________________'  '-'  '_________________________
+           ''''''-|---|----/  \==][^',_m_,'^][==/  \----|---|-''''''
+                           \__/        ~        \__/
+                                                         
+
+Hello and welcome to the group 17 flight simulator! 
+
+This file will explain to you step by step what each componenet of the 
+code does, and how to make the most out of it.  
+
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~                          
+                            
+                               User Guide                            
+                            
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~                          
+                            
+This intuitive simulation tool is designed to model the flight dynamics of a 
+small aircraft.Begin by executing the User_Interface.py file, which launches a 
+straightforward graphical user interface. Here, you will enter your desired 
+flight parameters, velocity (V) and flight path angle (γ), with the subsequent 
+step change. 
+
+Upon initiating the simulation, the software will processes your 
+data, yielding aerodynamic data. These visualizations detail key flight 
+dynamics, such as altitude and pitch angle over time, allowing for in-depth 
+analysis and design optimization.
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~                          
+                            
+                             Code Breakdown                            
+                            
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
+                           
+                            
+                           
+ 
+                
+ 
+    
+ 
+    
+ 
+    
+ 
+    
+ 
+    
