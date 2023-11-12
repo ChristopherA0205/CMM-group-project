31d52b64fb67f1fe5beb64be52b4e2bad8668cd7
faisalbindakhil <147502701+faisalbindakhil@users.noreply.github.com>
2023-11-12 01:17:11 +0000

    Update MainCode.py

--- MainCode.py
+++ MainCode.py
@@ -3,32 +3,34 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import integrate, optimize
-import pandas as pd
 import Trim_Conditions as trim
 import Constants_and_Data as c
 import Functions as F
+
 # Defining physical constants related to gravity, air properties, and aircraft characteristics
- 
+'''
+Please find descriptions of all the import files in the README file
+'''
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#                                           Part A1: Curve Fitting and Data Handling
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+''' 
+Please note that all the data handling and curve fitting is done in the Functions.py file. This section only includes
+the printing command which follows after all the computations.
+'''
 
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-                                             #Part A1
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-
-# Print the coefficients
+# Printing the aerodynamic coefficients
 print(f"CL0 = {F.CL0}, CLa = {F.CLa}, CLde = {F.CLde}, CM0 = {F.CM0}, CMa = {F.CMa}, CMde = {F.CMde}, CD0 = {F.CD0}, K = {F.K}")
 
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#                                            Part A2
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#                                            Part A2: Trimming the airplane
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-
- 
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+'''
+Before reading this part please find all relevant equations to this section in the Functions.py module under the Part A2 section acommpanied 
+with annotations explaining what each parameter does.
+'''
 
 # Function to calculate the trim conditions
 def calculate_trim_conditions(trimVelocity, trimGamma):
@@ -47,9 +49,15 @@ def calculate_trim_conditions(trimVelocity, trimGamma):
 
     return alpha, delta, theta, ub, wb, thrust
 
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#                                     Part A3
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#                                        Part A3: Equations of Motion Simulation
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+''' 
+Please find all the differential equations in the AircraftDynamics functions under the A3 section of the 
+Functions.py module. This section only contains simulation and plotting code, along with the ivp function to 
+solve for the differential equations of motion
+
+'''
 
 def DisplaySimulation_A3(Data, initialAltitude=0):
     t = Data.t  #Time array from simulation data
@@ -78,7 +86,7 @@ def DisplaySimulation_A3(Data, initialAltitude=0):
     plt.tight_layout()
     plt.show()  #Dispalying all the plots 
 
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 # Function to simulate the aircraft's response to control input changes during flight
 
@@ -111,7 +119,7 @@ def SystemControl(t, y, pitchTime, climbTime, trimParams, trimParams2, elevatorC
 
 # This function modifies control inputs at specified times during the flight
 
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 # Function to run the simulation using the initial conditions and user-defined parameters
 
@@ -146,9 +154,17 @@ def run_simulation(trimVelocity, trimGamma, t_end, pitchTime, climbTime, elevato
 
 run_simulation(trim.velocity_0, trim.gamma_0, 300, trim.pitchTime, trim.climbTime, trim.elevatorChange, trim.thrustChange, trim.initialAltitude)
 
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#                                               Part B1
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#                                          Part B1: Engineering Design Simulation
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+"""
+The aircraft simulation analyzes the aircraft's trim conditions over a range of velocities 
+and flight path angles. It generates arrays for velocity (V_values) and flight path angle (gamma_values), and 
+calculates corresponding thrust (T_values) and elevator deflection (δE_values) for each combination. These calculations 
+are based on the trim conditions determined by the calculate_trim_conditions function.The results are visualized using the 
+plot_trim_results function, which creates a series of subplots.
+"""
 
 # Generate arrays of velocity and flight path angle values within the specified range
 V_values = np.arange(trim.V_min, trim.V_max, trim.V_step)
@@ -204,15 +220,15 @@ def plot_trim_results(V_values, gamma_values, T_values, δE_values):
 plot_trim_results(V_values, gamma_values, T_values, δE_values)
 
 
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#                                     Part B2
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#                                            Part B2: Climb Analysis
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 def DisplaySimulation_B2(Data, initialAltitude):
     
     """
     Displays the simulation results for various aircraft state variables over time.
-    Data: Simulation data containing time and state variables
+    Data: Simulation data containing time and state variables found using the ivp function
     initialAltitude: The initial altitude of the aircraft
     """    
     # Labels for plotting the state variables 
@@ -225,7 +241,7 @@ def DisplaySimulation_B2(Data, initialAltitude):
         ('$x_E$ Horizontal Position vs Time', '$x_e$ [m]'),
         ('Altitude vs Time', 'Altitude h [m]')
     ]
-    # Extracting the state variables from the simulation data
+    # Extracting the state variables from the simulation data which was solved using the ivp function
     variables = [Data.y[2], Data.y[3], Data.y[1], Data.y[0], Data.y[4], -Data.y[5]]
     
     fig, ax = plt.subplots(3, 2, figsize=(12, 10))
@@ -239,50 +255,83 @@ def DisplaySimulation_B2(Data, initialAltitude):
     plt.tight_layout()
     plt.show()
 
-def sim_control_level_flight(t, y, trimParams):
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~    
+"""
+The section defines functions to control aircraft dynamics for level flight and climb, and combines 
+these controls based on flight phase.
+
+System_Control_Level_Flight: Calculates aircraft dynamics during level flight using delta (elevator angle) 
+and thrust from trim parameters.
+
+System_Control_Climb: Computes dynamics during climbing phase, similarly using delta and thrust, but from a 
+different set of trim parameters (trimParams2).
+
+Combined_Systems: Selects between level flight and climb control based on the current time 't' in relation to 
+pitchTime and climbTime. It uses level flight control before pitchTime and after pitchTime + climbTime, and climb 
+control during the interval between these times.
+"""
+
+    # This function determines the dynamics of the aircraft during level flight.
+def System_Control_Level_Flight(t, y, trimParams):
+    # Extracts the elevator angle ('delta') from the trim parameters.
     delta = trimParams[1]
+    # Extracts the thrust value from the trim parameters.
     thrust = trimParams[5]
+
     return F.AircraftDynamics(t, y, delta, thrust)
 
-def sim_control_climb(t, y, trimParams2):
+    # This function determines the dynamics of the aircraft during the climb phase.
+def System_Control_Climb(t, y, trimParams2):
     delta = trimParams2[1]
     thrust = trimParams2[5]
+    # Similar to Sim_Control_Level_Flight, this calls the AircraftDynamics
+
     return F.AircraftDynamics(t, y, delta, thrust)
+   
+ # This function combines the control for both level flight and climb phases,
+def Combined_Systems(t, y, trimParams, trimParams2, pitchTime, climbTime):
+    # switching between them based on the current time 't'.
 
-def combined_sim_control(t, y, trimParams, trimParams2, pitchTime, climbTime):
+    # Checks if the current time 't' is within the pitch and climb phase duration.
     if pitchTime <= t < pitchTime + climbTime:
-        return sim_control_climb(t, y, trimParams2)
+        # If within the climb phase, use the climb control parameters.
+        return System_Control_Climb(t, y, trimParams2)
     else:
-        return sim_control_level_flight(t, y, trimParams)
+        # If not in the climb phase, default to level flight control parameters.
+        return System_Control_Level_Flight(t, y, trimParams)
 
-
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 def Find_Climb_Time(trimVelocity, trimGamma, t_end, initialAltitude, maxAltitude, pitchTime, climbVelocity, climbGamma, climbTimeGuess=0, climbStep=0.5):
+    # Calculate initial and climbing trim conditions using given parameters.
     trimParams = calculate_trim_conditions(trimVelocity, trimGamma)
     trimParams2 = calculate_trim_conditions(climbVelocity, climbGamma)
 
+    # Initialize climb time guess and set the starting altitude.
     climbTime = climbTimeGuess
     finalAltitude = initialAltitude
 
+    # Continuously simulate the climb until reaching or exceeding the maximum altitude.
     while finalAltitude < maxAltitude:
+        # Perform the integration of the aircraft's dynamics over the specified time period,
+        # switching between trim conditions for level flight and climb as needed.
         y = integrate.solve_ivp(
-            lambda t, y: combined_sim_control(t, y, trimParams, trimParams2, pitchTime, climbTime),
+            lambda t, y: Combined_Systems(t, y, trimParams, trimParams2, pitchTime, climbTime),
             [0, t_end], 
             [0, trimParams[2], trimParams[3], trimParams[4], 0, -initialAltitude],
             t_eval=np.linspace(0, t_end, t_end * 50)
         )
+        # Update the final altitude achieved in the current iteration.
         finalAltitude = -y.y[5][-1]
+
+        # Increment the climb time if the desired altitude is not yet reached.
         if finalAltitude < maxAltitude:
             climbTime += climbStep
 
+    # Display the simulation results and print the total duration of the climb.
     DisplaySimulation_B2(y, initialAltitude)
     print(f"Climb Duration: {climbTime}s")
     return climbTime
 
-
-    DisplaySimulation_B2(y, initialAltitude)
-    
-    print(f"Climb Duration: {climbTime}s")
-    return climbTime
-
+# Executing the function with specific parameters to find the climb duration.
 climb_duration = Find_Climb_Time(trimVelocity=105, trimGamma=0, t_end=700, initialAltitude=1000, maxAltitude=2000, pitchTime=10, climbVelocity=105, climbGamma=np.deg2rad(2), climbTimeGuess=200, climbStep=1)
