540e3dce104e607aa040fc6b6136811100518539
keirbarbary <147502898+keirbarbary@users.noreply.github.com>
2023-11-14 11:32:42 +0000

    Update README.py

--- README.py
+++ README.py
@@ -30,8 +30,8 @@
 
 Hello and welcome to the group 17 flight simulator! 
 
-This file will explain to you step by step what each componenet of the code does, and how to make the most out of it.  
-The first file you should open it the UserInterface.py
+This file will explain to you step by step what each component of the code does, and how to make the most out of it.  
+The first file you should open is 'UserInterface.py'.
 
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~                           
@@ -39,17 +39,28 @@ The first file you should open it the UserInterface.py
                                                          User Guide                            
                              
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~                       
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~                            
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~      
 
-This simulation tool is designed to model the flight dynamics of a small aircraft. Begin by executing the 
-User_Interface.py file, which launches a straightforward graphical user interface. Here, you will enter your desired
-flight parameters, velocity (V) and flight path angle (γ), with the subsequent step change.   
 
-Upon initiating the simulation, the software will processes your data, yielding aerodynamic data. These visualizations 
-detail key flight dynamics, such as altitude and pitch angle over time, allowing for in-depth analysis and design 
-optimization.
+•	First open and then execute the file ‘UserInterface.py’, making sure that ‘MainCode.py’ is in the 
+  appropriate directory, to guarantee the expected operation of the User Interface (UI). Prior to running the script, please ensure 
+  that the 'PySimpleGUI' module is installed in your Python environment, as this is a requirement for the code to run.
+  
+•	Input values of ‘Velocity’ and ‘Flight Path Angle’ and press the ‘trim’ button to calculate dynamic equilibrium of the aircraft.
 
-IMPORTANT: Please find all of the main code which allows the user interface to work in the MainCode.py file 
+•	To simulate the response of the aircraft to changes, input the values shown (including ‘Initial Altitude’, the step changes in 
+  ‘Thrust’ and ‘Elevator Angle’, and the simulation duration before and after the step change’) and press the ‘evaluate’ button.
+
+This simulation tool is designed to model the flight dynamics of a small aircraft. When the 'trim' function is executed, it takes the user-defined
+inputs and calculates the parameters at which the aircraft is in perfect dynamic equilibrium, also known as the 'trim condition'. Subsequently,
+a handful of these values are printed on the graphical interface to show that the calculations were executed as expected. 
+
+The next section of the UI runs a simulation of how the flight dynamics change over time, starting at the previously 
+determined 'trim condition' and incorporating a user-defined 'step-change'. Upon initiating the evaluation, the software will 
+process your input, yielding aerodynamic data. These visualizations detail key flight dynamics, such as altitude and pitch angle 
+over time, allowing for in-depth analysis and design optimization.
+
+IMPORTANT: Please find all of the main code which allows the UI to work in the MainCode.py file
 
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~                           
@@ -59,9 +70,8 @@ IMPORTANT: Please find all of the main code which allows the user interface to w
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~                       
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  
 
-UserInterface.py: The first file you should open. It launches the graphical user interface used for testing and simulations.
+UserInterface.py: The first file you should open. It launches the graphical UI used for testing and simulations.
 All the code in this file runs by the simulations in the MainCode.py, which is then imported to this file for the user to test.
-
 
 MainCode.py: The file where all the computations are done, then imported to the UserInterface.py file.
 
@@ -75,7 +85,6 @@ code.
 
 Trim_Conditions.py: Although containing constants, this module is specific for trim condition values in order to import
 them in the later calculations of late part A and part B. 
-
 
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~                                                                                                           
@@ -107,7 +116,8 @@ plotted using the matplotlib.pyplot library.
  
 Part B1 conducts trim analysis over a range of velocities (V) and flight path angles (γ) to determine the necessary 
 thrust (T) and elevator angle (δE) adjustments for stable flight. Graphical outputs illustrate the dependencies of 
-T and δE on V and γ, adhering to constraints like positive thrust and viable control angles. 
+T and δE on V and γ, adhering to physical constraints such as a positive thrust value and viable control angles as well
+as constraints due to the range of the experimental data used to calculate aerodynamic coefficients.
 
 Part B2 simulates an aircraft's ascent from an altitude of 1000m to 2000m. This section  calculates the required trim 
 conditions at different stages of the climb, maintaining constant velocity while adjusting the flight path angle and 
@@ -123,7 +133,7 @@ member of the group.
 Section C provides a user-friendly interface for simulating flight conditions. The interface allows the user to input desired 
 flight trim parameters—velocity (V) and flight path angle (γ) and displays the resulting angle of attack (α), thrust (T), and 
 elevator angle (δE). Users can also input step changes in thrust and elevator angle, along with a total simulation time, to 
-observe the aircraft's time response.
+observe the aircraft's time response via plots of certain parameters against time. 
 
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
 
